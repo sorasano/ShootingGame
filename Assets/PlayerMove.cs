ef32c7d7cd4110938dbda7181bf56176d13c4c3f
SoraSano <k021g1331@g.neec.ac.jp>
2022-06-07 15:20:50 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //プレイヤーのワールド座標を取得
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += 0.01f;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= 0.01f;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.z += 0.01f;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.z -= 0.01f;
+        }
+
+        transform.position = new Vector3(pos.x,pos.y,pos.z);
+    }
+
+}
